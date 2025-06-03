4b1072c83a8ea81bcf7940f24685f53a3e7175b3
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-06-03 17:18:58 +0530

    Created Square component inside App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <div>
       Hello World
     </div>
+  );
+}
+
+function Square() {
+  return (
+    <button className='square'>X</button>
   );
 }
 
