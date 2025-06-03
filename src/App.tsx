b1e1a9f880fd21ab16698413eee319b78081f15c
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-06-03 17:33:01 +0530

    removing old component square

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,12 @@ import './App.css';
 function App() {
   return (
     <div>
-      <Square />
+      <Board />
     </div>
   );
 }
 
-function Square() {
-  return (
-    <>
-      <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
-      </div>
-
-      <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
-      </div>
-
-      <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
-      </div>
-
-    </>
-  );
-}
-
-const board: React.FC = () => {
+const Board: React.FC = () => {
   return (
     <>
       <div className="board-row">
