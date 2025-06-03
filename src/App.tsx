f25c3c9e6aedfda76baa71eafa55f7a562116c4f
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-06-03 17:45:20 +0530

    dynamic squares loading

--- src/App.tsx
+++ src/App.tsx
@@ -13,22 +13,35 @@ const Board: React.FC = () => {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square value="" />
+        <Square value="" />
+        <Square value="" />
       </div>
 
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value="" />
+        <Square value="" />
+        <Square value="" />
       </div>
 
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value="" />
+        <Square value="" />
+        <Square value="" />
       </div>
+
+    </>
+  );
+}
+
+type SquareProps = {
+  value: string
+}
+
+const Square: React.FC<SquareProps> = ({ value }) => {
+  return (
+    <>
+      <button className="square"> {value} </button>
     </>
   )
 }
