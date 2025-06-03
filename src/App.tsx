0e1d49366213caacd8600c744873c65c63369e3b
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-06-03 20:00:26 +0530

    gave state array indexe with elements to each Sqaure as value holder

--- src/App.tsx
+++ src/App.tsx
@@ -17,20 +17,20 @@ const Board: React.FC = () => {
     <>
       <div className="board-row">
         <Square value={array[0]} />
-        <Square value={array[0]} />
-        <Square value={array[0]} />
+        <Square value={array[1]} />
+        <Square value={array[2]} />
       </div>
 
       <div className="board-row">
-        <Square value={array[0]} />
-        <Square value={array[0]} />
-        <Square value={array[0]} />
+        <Square value={array[3]} />
+        <Square value={array[4]} />
+        <Square value={array[5]} />
       </div>
 
       <div className="board-row">
-        <Square value={array[0]} />
-        <Square value={array[0]} />
-        <Square value={array[0]} />
+        <Square value={array[6]} />
+        <Square value={array[7]} />
+        <Square value={array[8]} />
       </div>
 
     </>
