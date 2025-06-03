68cf2121720a45852ddd8e0c47efdc5a36b8368e
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-06-03 17:32:18 +0530

    creating new component board

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import type React from 'react';
 import './App.css';
 
 function App() {
@@ -31,6 +32,30 @@ function Square() {
 
     </>
   );
+}
+
+const board: React.FC = () => {
+  return (
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
+  )
 }
 
 export default App;
