9447e5f1f267729b3de13cd36c95a59582574e94
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-06-03 17:29:35 +0530

    testing the squares with numbers

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,21 @@ function Square() {
   return (
     <>
       <div className="board-row">
-        <button className="square"></button>
-        <button className="square"></button>
-        <button className="square"></button>
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
       </div>
 
       <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
         <button className="square"></button>
-        <button className="square"></button>
-        <button className="square"></button>
-      </div>
+      </div>6
 
       <div className="board-row">
-        <button className="square"></button>
-        <button className="square"></button>
-        <button className="square"></button>
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
       </div>
 
     </>
