52400b6656fec7fdecb4eb05ae814f1d7b79cb28
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-06-04 12:44:36 +0530

    adding handleClick instead of setSquare

--- src/App.tsx
+++ src/App.tsx
@@ -16,21 +16,21 @@ const Board: React.FC = () => {
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={setSquares} />
-        <Square value={squares[1]} onSquareClick={setSquares} />
-        <Square value={squares[2]} onSquareClick={setSquares} />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
       </div>
 
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={setSquares} />
-        <Square value={squares[4]} onSquareClick={setSquares} />
-        <Square value={squares[5]} onSquareClick={setSquares} />
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
       </div>
 
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={setSquares} />
-        <Square value={squares[7]} onSquareClick={setSquares} />
-        <Square value={squares[8]} onSquareClick={setSquares} />
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
       </div>
 
     </>
